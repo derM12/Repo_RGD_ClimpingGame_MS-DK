63229afcdf69f3b9298f1a1e93e08c49b35771d1
derM12 <derm12@hotmail.com>
2026-06-01 21:51:01 +0200

    Add Shopkeep character assets and animations
    Import Shopkeep character: added FBX, animation clips, controller, materials and textures under Assets/Mesh/Charakters/Shopkeep. Update scene and code: modified  ShopKeeper.cs to integrate the new character.

--- ClimpingGame/Assets/Scripts/ShopKeeper.cs
+++ ClimpingGame/Assets/Scripts/ShopKeeper.cs
@@ -6,9 +6,12 @@ public class Shopkeeper : MonoBehaviour
     public float interactRange = 2f;
 
     PlayerInventory inventory;
+    Animator animator;
+
 
     void Start()
     {
+            animator = GetComponent<Animator>();
         GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
         if (playerObj != null)
             inventory = playerObj.GetComponent<PlayerInventory>();
@@ -23,6 +26,12 @@ public class Shopkeeper : MonoBehaviour
         float distance = Vector3.Distance(transform.position, inventory.transform.position);
 
         if (distance <= interactRange && Keyboard.current.eKey.wasPressedThisFrame)
-            inventory.TradeRelicForRock();
+        {
+            if (inventory.TradeRelicForRock())
+            {
+                if (animator != null)
+                    animator.SetTrigger("Sell");
+            }
+        }
     }
 }
